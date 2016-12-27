ddfdd671d65e1f8b4da161dcb6828c440167887f
Dan Broadbent <dbroadbent85@gmail.com>
2016-12-27 13:31:27 -0700

    Add event details page

--- src/pages/event-details/event-details.ts
+++ src/pages/event-details/event-details.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the EventDetails page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-event-details',
   templateUrl: 'event-details.html'
 })
 export class EventDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  event: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.event = this.navParams.get('event');
+  }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EventDetailsPage');
+    console.log('Hello EventDetailsPage Page');
   }
 
 }
