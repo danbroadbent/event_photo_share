e643e9bb1cdd04f4c77a1e1e3cc7b356edc05755
Dan Broadbent <dbroadbent85@gmail.com>
2016-12-29 14:15:53 -0700

    Add password reset

--- src/pages/reset-password/reset-password.ts
+++ src/pages/reset-password/reset-password.ts
@@ -1,22 +1,68 @@
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthData } from '../../providers/auth-data';
+import { EmailValidator } from '../../validators/email';
 
-/*
-  Generated class for the ResetPassword page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-reset-password',
-  templateUrl: 'reset-password.html'
+  templateUrl: 'reset-password.html',
 })
 export class ResetPasswordPage {
+  public resetPasswordForm;
+  emailChanged: boolean = false;
+  passwordChanged: boolean = false;
+  submitAttempt: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ResetPasswordPage');
+  constructor(public authData: AuthData, public formBuilder: FormBuilder, public nav: NavController,
+    public loadingCtrl: LoadingController, public alertCtrl: AlertController) {
+
+    this.resetPasswordForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+    })
   }
 
+  elementChanged(input){
+    let field = input.inputControl.name;
+    this[field + "Changed"] = true;
+  }
+
+  resetPassword(){
+
+    this.submitAttempt = true;
+
+    if (!this.resetPasswordForm.valid){
+      console.log(this.resetPasswordForm.value);
+    } else {
+      this.authData.resetPassword(this.resetPasswordForm.value.email).then((user) => {
+        let alert = this.alertCtrl.create({
+          message: "We just sent you a reset link to your email",
+          buttons: [
+            {
+              text: "Ok",
+              role: 'cancel',
+              handler: () => {
+                this.nav.pop();
+              }
+            }
+          ]
+        });
+        alert.present();
+
+      }, (error) => {
+        var errorMessage: string = error.message;
+        let errorAlert = this.alertCtrl.create({
+          message: errorMessage,
+          buttons: [
+            {
+              text: "Ok",
+              role: 'cancel'
+            }
+          ]
+        });
+        errorAlert.present();
+      });
+    }
+  }
 }
