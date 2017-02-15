5d7948cd0612aa628a25b586c737907ae8cd654f
Dan Broadbent <dbroadbent85@gmail.com>
2017-02-14 17:34:48 -0700

    Add working email button to share

--- src/pages/share-event/share-event.ts
+++ src/pages/share-event/share-event.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ProfileData } from '../../providers/profile-data';
 
 /*
   Generated class for the ShareEvent page.
@@ -14,10 +15,19 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ShareEventPage {
 
   eventId: any;
+  emailCopy: string;
+  currentUsername: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public profileData: ProfileData) {
     this.eventId = navParams.data.eventId
+    profileData.getUserProfile().subscribe(data => {
+      this.currentUsername = data.username
+    })
+    this.emailCopy = `mailto:?to=&body=You've been invited!%0D%0A%0D%0ACheck out all the event photos here:%0D%0Ahttps://event-photo-share.firebaseapp.com/#/event/${this.eventId}&subject=Event Invitation`
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShareEventPage');
