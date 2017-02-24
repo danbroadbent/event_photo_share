57858c81dca2a495cf29cf625f6e7f05f3b6d0f4
Dan Broadbent <dbroadbent85@gmail.com>
2017-02-24 15:35:24 -0700

    WIP exif data is loading

--- src/pages/photo-uploader/photo-uploader.ts
+++ src/pages/photo-uploader/photo-uploader.ts
@@ -13,6 +13,8 @@ export class PhotoUploaderPage {
 
   files: any = {};
   eventId: any;
+  images= [];
+  metaData = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public photoData: PhotoData, public loadingCtrl: LoadingController) {
     this.eventId = this.navParams.get('eventId');
@@ -23,28 +25,62 @@ export class PhotoUploaderPage {
   }
 
   addPhotos(event) {
-    console.log('onChange');
-    this.files = event.srcElement.files;
-    console.log(this.files);
-    var output = document.getElementById("result");
-    output.innerHTML = "";   
+    this.files = event.srcElement.files
+    console.log(this.files)
+    this.loadPhotos()
+    // this.getMetaData()
+    // drawPhotosToCanvas()
+  }
+
+  loadPhotos() {
+    var output = document.getElementById("output")
+    output.innerHTML = ""
     for(var i = 0; i< this.files.length; i++){
-        var file = this.files[i];
-        if(file.type.match('image.*')){
-          var picReader = new FileReader();
-          picReader.onload = function (e : any) {
-          var div = document.createElement("div");       
-          div.innerHTML = "<img class='thumbnail' src='" + e.target.result + "'" +
-                  "title='" + e.target.name + "'/>";
-          output.insertBefore(div,null);
-        }   
-        picReader.readAsDataURL(file);
-        this.addUploadButton()
-      }  else {
-      alert("You can only upload image files.");
+      var file = this.files[i];
+      if(file.type.match('image.*')){
+        var img = document.createElement("img");
+        output.appendChild(img); // Assuming that "preview" is the div output where the content will be displayed.
+        var reader = new FileReader();
+        reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+        reader.readAsDataURL(file);
+        this.images[i] = img
+        EXIF.getData(file, function () {
+        var data = EXIF.getAllTags(this);
+        console.log(data)
+      })
       }
     }
+    console.log(this.images)
   }
+
+  // getMetaData() {
+  //       debugger
+  //   for(var i = 0; i < this.images.length; i++){
+  //     var image = this.images[i]
+      
+  //   }
+  // }
+
+  // addPhotosOld(event) {
+  //   this.files = event.srcElement.files;
+  //   console.log(this.files);
+  //   for(var i = 0; i< this.files.length; i++){
+  //     var file = this.files[i];
+  //     if(file.type.match('image.*')){
+  //       var picReader = new FileReader();
+  //       var img = document.createElement("img");
+  //       img.classList.add("obj");
+  //       img.file = file
+  //       picReader.onload = function (e : any) {
+  //         img.src = e.target.result
+  //         }   
+  //       picReader.readAsDataURL(file);
+  //       this.addUploadButton()
+  //     } else {
+  //     alert("You can only upload image files.");
+  //     }
+  //   }
+  // }
 
   addUploadButton() {
     document.getElementById("uploadButton").removeAttribute("hidden")
